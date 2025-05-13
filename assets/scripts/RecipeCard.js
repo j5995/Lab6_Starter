fe53342a2f5613e99070974b82323f728c62bc16
j5995 <wangtianyi63@gmail.com>
2025-05-13 02:31:51 -0700

    fixed links

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -131,7 +131,7 @@ class RecipeCard extends HTMLElement {
 		//           Remember to replace all the placeholders in the template with the data passed in.
 		//           i.e. imgSrc, titleLnk, etc
 		article.innerHTML = `
-  <img src="${data.imgSrc}"
+  <img src=${data.imgSrc}
     alt="${data.imgAlt}">
   <p class="title">
     <a href="${data.titleLnk}">"${data.titleTxt}"</a>
